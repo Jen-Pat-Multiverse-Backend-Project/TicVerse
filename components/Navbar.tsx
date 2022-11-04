7624ec7fd325cfb4ada1dea72bd5808656c2d1ac
Jen Perez <jenperez613@icloud.com>
2022-11-04 13:55:57 -0400

    set up google login and auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
-import { AiOutlineLogout } from 'react-icons/ai'
-import {BiSearch} from 'react-icons/bi'
-import { IoMdAdd } from 'react-icons/io'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
 
-import Logo from '../utils/tiktik-logo.png'
-
+import Logo from '../utils/tiktik-logo.png';
+import { createOrGetUser } from '../utils';
 
 const Navbar = () => {
+  const user = false;
+
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -23,8 +25,19 @@ const Navbar = () => {
           />
         </div>
       </Link>
+      <div>SEARCH</div>
+      <div>
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+            <GoogleLogin
+              onSuccess={(response) => createOrGetUser(response)}
+              onError={() => console.log('error')}
+            />
+        )}
+      </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
+import { GoogleOAuthProvider} from '@react-oauth/google'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [isSSR, setIsSSR] = useState(true);
@@ -14,7 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   if (isSSR) return null;
 
   return (
-    <div>
+    <GoogleOAuthProvider clientId={`${ process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN }`}>
       <Navbar />
       <div className="flex gap-6 md:gap-20">
         <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
@@ -24,7 +25,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   );
 }
 
