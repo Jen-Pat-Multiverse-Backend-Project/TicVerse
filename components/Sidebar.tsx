ea5faeee663250faa1ef1e827b8ac3543562600a
Jen Perez <jenperez613@icloud.com>
2022-11-11 02:03:58 -0500

    details, comments, fetchUsers, API routing

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,14 +7,18 @@ import { ImCancelCircle } from 'react-icons/im';
 import Footer from './Footer';
 import SuggestedAccounts from './SuggestedAccounts';
 import Discover from './Discover';
+import useAuthStore from '../store/authStore';
 
 const Sidebar: NextPage = () => {
   const [showSidebar, setShowSidebar] = useState(true);
+  const { pathname } = useRouter();
+  const { fetchAllUsers, allUsers }: any = useAuthStore();
 
-  const normalLink =
+  const activeLink =
     'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#F51997] rounded';
 
-  const userProfile = false;
+  const normalLink =
+    'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold rounded';
 
   return (
     <div>
@@ -25,22 +29,40 @@ const Sidebar: NextPage = () => {
         {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
       </div>
       {showSidebar && (
-        <div className="w-40 xl:w-400 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
+        <div className="w-20 xl:w-[400px] flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
           <div className="xl:border-b-2 border-gray-200 xl:pb-4">
             <Link href="/">
-              <div className={normalLink}>
+              <div
+                className={pathname === '/' ? activeLink : normalLink}
+              >
                 <p className="text-2xl">
                   <AiFillHome />
                 </p>
-                <span className="capitalize text-xl hidden xl:block">
+                <span className="text-xl hidden xl:block">
                   For You
                 </span>
               </div>
             </Link>
           </div>
 
+          {/*  {!userProfile && (
+            <div className="px-2 py-4 hidden xl:block">
+              <p className="text-gray-400">
+                Log in to like and comment on videos
+              </p>
+              <div className="pr-4">
+                <button className="bg-white text-lg text-[#F51997] border-[1px] border-[#F51997] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[]#F51997 cursor-pointer">
+                  Google Login Placeholder
+                </button>
+              </div>
+            </div>
+          )} */}
+
           <Discover />
-          <SuggestedAccounts />
+          <SuggestedAccounts
+            fetchAllUsers={fetchAllUsers}
+            allUsers={allUsers}
+          />
           <Footer />
         </div>
       )}
